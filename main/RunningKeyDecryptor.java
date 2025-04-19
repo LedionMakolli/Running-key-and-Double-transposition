a857d224f61a5d8b95b1d05be81916d5f0cc42cd
jonaguri <jonaguri484@gmail.com>
2025-04-20 00:02:25 +0200

    Create RunningKeyDecryptor.java
    Implementimi i funksionit për dekriptim

---
+++ main/RunningKeyDecryptor.java
@@ -0,0 +1,27 @@
+
+public class RunningKeyDecryptor {
+
+    public String DecryptRunningKey(String cipherText, String Libri) {
+        int cipherTextSize = cipherText.length();
+        String key = Libri.substring(0, cipherTextSize);
+
+        char[] cipherTextArray = cipherText.toCharArray();
+        char[] keyArray = key.toCharArray();
+        char[] plainTextArray = new char[cipherTextSize];
+
+        for (int i = 0; i < cipherTextSize; i++) {
+            if (Character.isUpperCase(cipherTextArray[i]) && Character.isUpperCase(keyArray[i])) {
+                plainTextArray[i] = (char) ((((cipherTextArray[i] - 'A') - (keyArray[i] - 'A') + 26) % 26) + 'A');
+            } else if (Character.isUpperCase(cipherTextArray[i])) {
+                plainTextArray[i] = (char) ((((cipherTextArray[i] - 'A') - (keyArray[i] - 'a') + 26) % 26) + 'A');
+            } else if (Character.isUpperCase(keyArray[i])) {
+                plainTextArray[i] = (char) ((((cipherTextArray[i] - 'a') - (keyArray[i] - 'A') + 26) % 26) + 'a');
+            } else {
+                plainTextArray[i] = (char) ((((cipherTextArray[i] - 'a') - (keyArray[i] - 'a') + 26) % 26) + 'a');
+            }
+        }
+
+        return new String(plainTextArray);
+    }
+}
+
