1475c2821a4154febe3fe27f33680d49ab4c4359
Kimete Sadiku <kimetesadiku8@gmail.com>
2025-04-19 15:53:22 +0200

    Krijimi i matrices

--- main/DoubleTranspositionDecryptor.java
+++ main/DoubleTranspositionDecryptor.java
@@ -1,1 +1,20 @@
 
+   //Krijimi i matrices nga teksti i enkriptuar
+    private static char[][] createMatrix(String text, int rows, int cols) {
+        char[][] matrix = new char[rows][cols];
+        int index = 0;
+        text = text.replace(" ",""); 
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (index < text.length()) {
+                    matrix[i][j] = text.charAt(index++);
+                } else {
+                    matrix[i][j] = 'X'; 
+                }
+            }
+        }
+
+        return matrix;
+    }
+
