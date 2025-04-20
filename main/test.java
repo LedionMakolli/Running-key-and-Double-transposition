24e59b9e2674c488528b722ea990d7629cd2106c
Kimete Sadiku <kimetesadiku8@gmail.com>
2025-04-20 18:13:49 +0200

    pjesa e testimit

--- main/test.java
+++ main/test.java
@@ -2,7 +2,14 @@
 
 
 public class test {
+ //Funksioni per testim-DoubleTranspositionDecryptor
     public static void main(String[] args) {
+        String encryptedText = "LORDXXLHELOW";
+        int[] rowKey = {1, 0}; 
+        int[] colKey = {2, 0, 1, 3, 4, 5};
 
+        String decrypted = decrypt(encryptedText, rowKey, colKey);
+        System.out.println("Teksti i dekriptuar: " + decrypted);
     }
-}
+    }
+
