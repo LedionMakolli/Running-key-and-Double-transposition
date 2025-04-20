a68478813e82aee94628d83ef27d0404daac3930
Leart <leartbalidemaj12@gmail.com>
2025-04-20 02:46:25 +0200

    Funksioni i enkriptimit

--- main/DoubleTransposition.java
+++ main/DoubleTransposition.java
@@ -20,4 +20,37 @@ public class DoubleTransposition {
         }
         return matrix;
     }
+
+    // Funksioni i enkriptimit
+    public static String encrypt(String plaintext, int[] rowKey, int[] colKey) {
+        plaintext = plaintext.replace(" ", "").toUpperCase();
+        int rows = rowKey.length;
+        int cols = colKey.length;
+        
+        char[][] matrix = createMatrix(plaintext, rows, cols);
+
+        // Ri-rendit rreshtat
+        char[][] rowTransposed = new char[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            rowTransposed[i] = matrix[rowKey[i]];
+        }
+
+        // Ri-rendit kolonat
+        char[][] colTransposed = new char[rows][cols];
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < rows; i++) {
+                colTransposed[i][j] = rowTransposed[i][colKey[j]];
+            }
+        }
+
+        // Nderton tekstin e enkriptuar
+        StringBuilder encrypted = new StringBuilder();
+        for (char[] row : colTransposed) {
+            for (char c : row) {
+                encrypted.append(c);
+            }
+        }
+
+        return encrypted.toString();
+    }
 }
